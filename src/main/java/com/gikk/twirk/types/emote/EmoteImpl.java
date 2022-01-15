f1073e48472a68566c2ec2d44253f128eee0fb78
gikkman <gikk@gikkman.com>
2022-01-15 22:44:17 +0100

    Remove deprecated getEmoteID method
    This method was deprecated when Twitch changed emote
    IDs to be Strings, but we kept it to not randomly break
    APIs. But since we are already changing an API in 0.7.0
    we might as well remove this method too.

--- src/main/java/com/gikk/twirk/types/emote/EmoteImpl.java
+++ src/main/java/com/gikk/twirk/types/emote/EmoteImpl.java
@@ -7,18 +7,12 @@ import java.util.LinkedList;
 class EmoteImpl implements Emote{
 	private final static String EMOTE_URL_BASE 	= "http://static-cdn.jtvnw.net/emoticons/v1/";
 
-	private int emoteID;
 	private String emoteIDString;
 	private final LinkedList<EmoteIndices> indices = new LinkedList<>();
 	private String pattern;
 
 	public EmoteImpl setEmoteIDString(String emoteIDString){
 		this.emoteIDString = emoteIDString;
-		return this;
-	}
-
-	public EmoteImpl setEmoteID(int emoteID){
-		this.emoteID = emoteID;
 		return this;
 	}
 
@@ -30,11 +24,6 @@ class EmoteImpl implements Emote{
 	public EmoteImpl addIndices(int begin, int end){
 		this.indices.add( new EmoteIndices(begin, end) );
 		return this;
-	}
-
-	@Override
-	public int getEmoteID() {
-		return emoteID;
 	}
 
 	@Override
@@ -55,7 +44,7 @@ class EmoteImpl implements Emote{
 
     @Override
 	public String getEmoteImageUrl(EMOTE_SIZE imageSize){
-		return EMOTE_URL_BASE + emoteID + imageSize.value;
+		return EMOTE_URL_BASE + emoteIDString + imageSize.value;
 	}
 
 
